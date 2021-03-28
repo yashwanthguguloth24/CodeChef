101784c498df7ceedd5baadc2332d8c5ab4637e3
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-03-28 23:39:18 +0530

    Update college_life5.py

--- March CodeChef Starters 2021 Division 3/college_life5.py
+++ March CodeChef Starters 2021 Division 3/college_life5.py
@@ -1,18 +1,40 @@
 # Yashwanth
 # 28/3/2021
 
-
 for _ in range(int(input())):
-    N,M = [int(x) for x in input().split()]
-    arr = [int(x) for x in input().split()]
-    flag_arr = [0]*M
-    for i in range(N):
-        flag_arr[arr[i]-1] = 1
-    flag = False
-    for j in range(M):
-        if flag_arr[j] == 0:
-            flag = True
-    if flag == True:
-        print('Yes')
-    else:
-        print('No')
+    NFoot,MCric = [int(x) for x in input().split()]
+    F = [int(x) for x in input().split()]
+    C = [int(x) for x in input().split()]
+    iFoot = 0
+    jCric = 0
+    flag = 0 # 0-foot 1-Cric
+    num_switch = 0
+    while iFoot < NFoot and jCric < MCric:
+        if F[iFoot] < C[jCric]:
+            if flag == 1:
+                num_switch += 1
+            flag = 0
+            iFoot += 1
+        else:
+            if flag == 0:
+                num_switch += 1
+            flag = 1
+            jCric += 1
+            
+    while iFoot < NFoot:
+        if flag == 0:
+            break
+        else:
+            num_switch += 1
+            flag = 0
+            iFoot += 1
+            
+    while jCric < MCric:
+        if flag == 1:
+            break
+        else:
+            num_switch += 1
+            flag = 1
+            jCric += 1  
+            
+    print(num_switch)
